b0c25262e1afc3a7a1ff2d1cb985eccbe41671c6
Guy Coen <gcoen@cirrusled.com>
2023-05-18 08:07:31 +0800

    Reduce CPU load

--- monitor.py
+++ monitor.py
@@ -1,32 +1,29 @@
-import argparse
-import select
-import signal
-import socket
-import sys
+from select import select
+from signal import signal
+from socket import socket
+from sys import exit
 from os.path import exists
 from os import system 
-
 from datetime import datetime
+from time import sleep
 
 def hexstring(hexarray):
   return ''.join('{:02x}'.format(x) for x in hexarray)
 
 def sigint_handler(signal, frame):
-  sys.exit(0)
+  exit(0)
 
 def start(eth, timeout):
   last = datetime.now()
   panels = set()
 
-  first_packet = True
 
-  s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+  s = socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
   s.bind((eth, 0))
   while True:
     r, _, _, = select.select([s], [], [], 1.)
     if len(r) > 0:
       packet = s.recv(1500)
-      dst = packet[:6]      # Destination MAC address
       src = packet[6:12]    # Source MAC address
       cmd = packet[14]      # Nucleus CMD
 
@@ -38,58 +35,24 @@ def start(eth, timeout):
         tmp = packet[55:57]   # Temperature
         fps = packet[141:143] # FPS rate
 
-        temp = int.from_bytes(tmp, 'little')
+        temp = int.from_bytes(tmp, 'little') 
         frat = int.from_bytes(fps, 'little')
-
-        if first_packet:
-          temp_min = temp_max = temp 
-          frat_min = frat_max = frat
-          first_packet = False
-
-        if temp < temp_min:
-          temp_min = temp
-        elif temp > temp_max:
-          temp_max = temp
-
-        if frat < frat_min:
-          frat_min = frat
-        elif frat > frat_max:
-          frat_max = frat
 
         now = datetime.now()
         if (now - last).seconds >= timeout:
-          shell = "rrdtool updatev panels.rrd N:{}:{}:{}:{}:{}".format(len(panels),temp_min,temp_max,frat_min,frat_max)
+          shell = "rrdtool updatev panels.rrd N:{}:{}:{}:{}:{}".format(len(panels),float(temp)/10,frat)
           system(shell)
-          print('Number of detected panels  : {}'.format(len(panels)))
-          print('Temperature range          : {} - {}'.format(temp_min, temp_max))
-          print('FPS range                  : {} - {}'.format(frat_min, frat_max))
           panels.clear()
-          first_panel=True
           last = now
+          sleep(30)
 
 if __name__ == '__main__':
-  signal.signal(signal.SIGINT, sigint_handler)
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-i', '--interface', type=str, help='Ethernet interface to monitor.')
-  parser.add_argument('-t', '--time-interval', type=int, default=1, help='The time interval to monitor the detected panels.')
-  args = parser.parse_args()
+  signal(signal.SIGINT, sigint_handler)
   if not exists ("panels.rrd"):
     system("rrdtool create panels.rrd --step 60 "
            "DS:detected:GAUGE:60:U:U "
-           "DS:temp_min:GAUGE:60:U:U "
-           "DS:temp_max:GAUGE:60:U:U "
-           "DS:frat_min:GAUGE:60:U:U "
-           "DS:frat_max:GAUGE:60:U:U "
+           "DS:temp:GAUGE:60:U:U "
+           "DS:frat:GAUGE:60:U:U "
            "RRA:AVERAGE:0.5:1:1000")
 
-  if (args.interface == ""):
-    iface = "enp5s0"
-  else:
-    iface = args.interface
-
-  if (args.time_interval == ""):
-    interval = 30
-  else:
-    interval = args.time_interval
-
-  start(iface, interval)
+  start("enp5s0", 2)
